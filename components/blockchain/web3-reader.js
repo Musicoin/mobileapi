42122b266ac043ebcfb8a9f51aeb2602e5ed7a82
Dan Phifer <dan.phifer@gmail.com>
2016-12-01 17:52:10 +0800

    * hard coding in a mapping between owner address and profile address, for testing.

--- components/blockchain/web3-reader.js
+++ components/blockchain/web3-reader.js
@@ -40,7 +40,42 @@ Web3Reader.prototype.loadLicense = function(licenseAddress) {
 };
 
 Web3Reader.prototype.getArtist = function(artistAddress, output) {
+  artistAddress = this.lookupProfileAddress(artistAddress);
   return this.loadContract(artistAddress, artistAbi, output);
+};
+
+Web3Reader.prototype.lookupProfileAddress = function(ownerAddress) {
+  // TODO: Hack
+  var lookup = {};
+  lookup['0x6cf4e1d23f69d9a7df1e3e8b3f7f674c7c93a1fd'] = {
+    name: 'Lobo Loco',
+    profileAddress: '0xe0288b127fbe0f5BfCa4254A6859F139fa06413E'
+  };
+
+  lookup['0x1269f9bd6aa7d3a4ab4db83dda2a439c1ef06ecd'] = {
+    name: 'Xenobia',
+    profileAddress: '0x85E505F358CD600126650128F8e6B6be52Ec9Fe4'
+  };
+
+  lookup['0xd9b87b28449de9a45560fadace31300fcc50e68b'] = {
+    name: 'Tofuku',
+    profileAddress: '0xb3B15E688844151C9C0aEb9Ea0647eFbf164546d'
+  };
+
+  lookup['0xd9b87b28449de9a45560fadace31300fcc50e68b'] = {
+    name: 'Porceline',
+    profileAddress: '0x9aBc7B43868161BF6f73f41DaE6854d2191a0A28'
+  };
+
+  lookup['0xd9b87b28449de9a45560fadace31300fcc50e68b'] = {
+    name: 'Dan Phifer',
+    profileAddress: '0xb4dbe3aF8E1d37963Cc782773bDC1dCcC120E7c6'
+  };
+
+  if (lookup[ownerAddress]) {
+    return lookup[ownerAddress].profileAddress;
+  }
+  return '0xb4dbe3aF8E1d37963Cc782773bDC1dCcC120E7c6';
 };
 
 Web3Reader.prototype.loadWork = function(workAddress, output) {
