1f31e942ce75ad5a5495757b9220550e9384dee9
Varunram <vrg2009@ymail.com>
2018-09-11 22:42:59 +0530

    Fix release + user, now onto user + release in search

--- rest-api/app/Controllers/GlobalController.js
+++ rest-api/app/Controllers/GlobalController.js
@@ -12,34 +12,91 @@ const mongoose = require('mongoose');
 
 class GlobalController {
   async search(Request, Response) {
+    console.log("CALLING SEARCH ENDPOINT");
     // need to add a direct map from various fields here. TODO. As of now, the
     // endpoint works though
-    try {
-      let user = await User.findOne(Request.body);
-      let releases = await Release.find(Request.body);
-      let ReleasesArray = [];
-      let ResponseInstance = {};
+    let releases = await Release.find(Request.body);
+    let ReleasesArray = [];
+    let ResponseInstance = {};
 
-      if (releases.length > 0) {
-        for (let track of releases) {
+    var ctr = 0;
+    if (releases.length > 0) {
+      for (var i = 0; i < releases.length; i++) {
+        console.log("I+", i);
+        if (typeof releases[i] != 'undefined') {
+          ctr = i;
           ReleasesArray.push({
-            title: track.title,
-            link: 'https://musicion.org/nav/track/' + track.contractAddress,
-            pppLink: track.tx,
-            genres: track.genres,
-            author: track.artistName,
-            authorLink: 'https://musicoin.org/nav/artist/' + track.artistAddress,
-            trackImg: track.imageUrl,
-            trackDescription: track.description,
-            directTipCount: track.directTipCount,
-            directPlayCount: track.directPlayCount
+            title: releases[i].title,
+            link: 'https://musicion.org/nav/track/' + releases[i].contractAddress,
+            pppLink: releases[i].tx,
+            genres: releases[i].genres,
+            author: releases[i].artistName,
+            authorLink: 'https://musicoin.org/nav/artist/' + releases[i].artistAddress,
+            trackImg: releases[i].imageUrl,
+            trackDescription: releases[i].description,
+            directTipCount: releases[i].directTipCount,
+            directPlayCount: releases[i].directPlayCount
           });
         }
       }
 
+      // now we have some releases, lets find the artist from here
+      if (ctr >= 0) {
+        let artist = User.findOne({
+          profileAddress: {
+            $exists: true,
+            $ne: null
+          }
+        }).where({
+          profileAddress: releases[ctr].artistAddress
+        })
+        console.log("ARTS", artist);
+        if (typeof artist != 'undefined' || typeof artist != 'null') {
+          ResponseInstance = {
+            totalTrackTips: 0,
+            totalFollowers: 0,
+            totalReleases: 0,
+            totalPlays: 0
+          };
+          // loop through the releases to get the relevant stats
+          for (var i = 0; i < releases.length; i++) {
+            if (typeof releases[ctr].directTipCount != 'undefined') {
+              ResponseInstance.totalTrackTips += releases[ctr].directTipCount;
+              ResponseInstance.totalReleases += 1; // count all track wiht defined number of tips
+            }
+            if (typeof releases[ctr].directPlayCount != 'undefined') {
+              ResponseInstance.totalPlays += releases[ctr].directPlayCount;
+            }
+            if (typeof releases[ctr].directPlayCount != 'undefined') {
+              ResponseInstance.totalReleases += 1;
+            }
+          }
+          ResponseInstance.name = releases[ctr].artistName;
+          ResponseInstance.artistURL = 'https://musicoin.org/nav/artist/' + releases[ctr].artistAddress;
+          ResponseInstance.totalFollowers = artist.followerCount;
+        }
+        Response.send({
+          success: true,
+          data: {
+            user: ResponseInstance,
+            releases: ReleasesArray
+          }
+        });
+        return
+      }
+      Response.send({
+        success: true,
+        data: {
+          releases: ReleasesArray
+        }
+      });
+      return
+    } else {
+      console.log("GOIGN THE USER WYA")
+      let user = await User.findOne(Request.body);
       if (typeof user != 'undefined' || typeof user != 'null') {
         let releases = await Release.find({
-          artistAddress: user.profileAddress
+          artistAddress: user.profileAddress || ''
         });
 
         ResponseInstance = {
@@ -71,11 +128,6 @@ class GlobalController {
           user: ResponseInstance,
           releases: ReleasesArray
         }
-      })
-    } catch (Error) {
-      Response.send({
-        success: false,
-        error: Error.message
       })
     }
   }
