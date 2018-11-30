161a3ea0c0afb94f37ee599498536ac6dc083a63
River <riverfor@gmail.com>
2018-11-30 17:18:31 +0800

    adjust code struct of apollographql

--- apollo/server.js
+++ apollo/server.js
@@ -2,7 +2,7 @@ const {
   ApolloServer
 } = require('apollo-server-express');
 const typeDefs = require('./schemas');
-const resolvers = require('./resolvers');
+const resolvers = require('./resolver');
 
 const Release = require('../db/core/release');
 
