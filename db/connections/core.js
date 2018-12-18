3c941996601d5eeec502b81c94a3b7c64c4a973d
River <riverfor@gmail.com>
2018-12-18 17:36:36 +0800

    change mongoose version

--- db/connections/core.js
+++ db/connections/core.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
 const url = process.env.MONGO_ENDPOINT?process.env.MONGO_ENDPOINT+"/musicoin-org":'mongodb://localhost/musicoin-org';
-module.exports = mongoose.connect(url);
+mongoose.connect(url);
+module.exports = mongoose;
