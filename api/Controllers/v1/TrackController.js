504900fbfb1e07da97b7fb00ede9baa9a92fdd0e
River <riverfor@gmail.com>
2019-01-10 18:21:48 +0800

    adjust trackcontroller to fetch license key

--- api/Controllers/v1/TrackController.js
+++ api/Controllers/v1/TrackController.js
@@ -1,15 +1,38 @@
 const LicenseKey = require('../../../db/core/key');
 const LicenseModule = require('../../Kernel').musicoinCore.getLicenseModule();
 const MediaProvider = require('../../../utils/media-provider-instance');
+const request = require('request');
+const CLIENT_ID = "OLDjonAeYPx6ZVKaMD3a";
+const logger = require('../../../utils/Logger');
+
+function getLicenseKey(licenseAddress) {
+  return new Promise((resolve, reject)=>{
+    request({
+      url: `http://35.186.250.94/license/ppp/${licenseAddress}`,
+      json: true,
+      headers: {
+        clientID: CLIENT_ID
+      }
+    }, function (error, response, result) {
+      if (error) {
+        logger.debug("get license key error: ",error);
+        reject(error);
+      }else if (response.statusCode != 200) {
+        reject("not found");
+      }else{
+        resolve(result);
+      }
+    })
+  })
+}
 
 async function downloadTrack(Request, Response) {
   const address = Request.params.address;
 
   try {
-    const licenseKey = await LicenseKey.findOne({
-      licenseAddress: address
-    }).exec();
-  
+    const licenseKey = await getLicenseKey(address);
+    
+    logger.debug("license key:",licenseKey);
     if (!licenseKey) {
       return Response.status(400).json({
         error: `licenseKey not found: ${address}`
