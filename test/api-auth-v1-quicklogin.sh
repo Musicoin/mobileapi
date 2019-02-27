107f323af5dcc968b237ec7e42110d83f2e07d8f
River <riverfor@gmail.com>
2019-02-27 23:42:17 +0800

    Added:     Test's README

---
+++ test/api-auth-v1-quicklogin.sh
@@ -0,0 +1,7 @@
+curl --location --request POST "https://t2.musicoin.org/api/v1/auth/quicklogin" \
+  --header "Content-Type: application/json" \
+  --data "{
+    \"email\": \"river6@gmail.com\",
+    \"password\": \"123456\"
+}"
+
--- test/api-global-v1-search.sh
+++ test/api-global-v1-search.sh
@@ -2,8 +2,8 @@
 curl --location --request POST "https://t2.musicoin.org/api/v1/search?email=$1&accessToken=$2" \
   --header "Content-Type: application/json" \
   --data "{
-    \"keyword\": \"dong\",
-    \"limit\": \"2\",
+    \"keyword\": \"Diego\",
+    \"limit\": \"20\",
     \"skip\": \"0\"
 }"
 
