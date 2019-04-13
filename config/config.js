6deb60227279dd3a80b1488e28c58ef5d87831f6
River <riverfor@gmail.com>
2019-04-14 00:38:13 +0800

    Add google key

--- config/config.js
+++ config/config.js
@@ -59,6 +59,7 @@ function getDefaultKeyValueConfig() {
     forwardingAddress: env.FORWARDING_ADDRESS || '0x0',
     debug: env.DEBUG || 0,   // should be change to false by default
     itunesSharedSecret: env.ITUNES_SHARED_SECRET || ''
+    googlePubKey: env.GOOGLE_PUB_KEY || ''
   };
 }
 
